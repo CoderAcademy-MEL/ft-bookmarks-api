a3f2c85864bfdd6f2455ced71c730677f6da7577
Harrison Malone <harrison.malone2@gmail.com>
2020-07-01 16:14:25 +1000

    slight changes to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080', 'https://amazing-blackwell-8c5a0a.netlify.app/'
+    origins 'http://localhost:8080', 'https://amazing-blackwell-8c5a0a.netlify.app'
 
     resource '*',
       headers: :any,
