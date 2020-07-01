08232244c786a18632b7145c3d95313a5458c1ad
Harrison Malone <harrison.malone2@gmail.com>
2020-07-01 12:31:21 +1000

    implemented jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :bookmarks
+  post "/login", to: "user_token#create"
+  post "/sign-up", to: "users#create"
+  get "/status", to: "status#index"
+  # => http://localhost:3000/status
+  get "/status/user", to: "status#user"
 end
