70d24deb13988c6aae84170481d9d3417a6b2da8
Harrison Malone <harrison.malone2@gmail.com>
2020-07-20 16:45:37 +1000

    added an update image endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :bookmarks
+  put "/bookmarks/image/:id", to: "bookmarks#update_image"
   post "/login", to: "user_token#create"
   post "/sign-up", to: "users#create"
   put "/users", to: "users#update"
