a748c0aa417fb6fdd6fbc8062ac2970da7138dd3
Harrison Malone <harrison.malone2@gmail.com>
2020-07-20 13:12:57 +1000

    in index sending back all bookmarks with the image url attached, needed to add method that generates urls, also changed create action to send back image

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,7 +3,7 @@ class BookmarksController < ApplicationController
   before_action :set_bookmark, only: %i[show update destroy]
 
   def index
-    bookmarks = current_user.bookmarks.order(id: 'asc')
+    bookmarks = generate_image_urls(current_user.bookmarks.with_attached_image)
     render json: { bookmarks: bookmarks, current_user: current_user.email }
   end
 
@@ -13,7 +13,7 @@ class BookmarksController < ApplicationController
 
   def create
     bookmark = current_user.bookmarks.create(bookmark_params)
-    render json: bookmark, status: :created
+    render json: { bookmark: bookmark, image: url_for(bookmark.image) }, status: :created
   end
 
   def update
@@ -29,10 +29,20 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:title, :description, :url)
+    params.require(:bookmark).permit(:title, :description, :url, :image)
   end
 
   def set_bookmark
     @bookmark = Bookmark.find(params[:id])
+  end
+
+  def generate_image_urls(bookmarks)
+    bookmarks.map do |bookmark|
+      if bookmark.image.attached?
+        bookmark.attributes.merge(image: url_for(bookmark.image))
+      else
+        bookmark
+      end
+    end
   end
 end
